f29a28d75e927ad904b50ca54c01c6b2ce54b67e
canyonwan <canyonwan@126.com>
2021-11-19 13:18:53 +0800

    🎉 init project

---
+++ README.md
@@ -0,0 +1,20 @@
+# tform
+
+A easy, extensible and dynamic flutter form framework. Support for custom selectors, validators and widgets. Support form verification, insert, delete and so on.
+
+## Installing
+Add this to your package's pubspec.yaml file:
+
+```yaml
+dependencies:
+  tform: ^1.0.1
+```
+
+## Example
+![avatar](./assets/demo.gif)
+
+## Build Form Rows
+![avatar](./assets/carbon_rows.png)
+
+## Build Form
+![avatar](./assets/carbon_page.png)
---
+++ lib/form/form_cell.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'form.dart';
+import 'form_field.dart';
+import 'form_row.dart';
+
+class TFormCell extends StatefulWidget {
+  TFormCell({Key? key, this.row}) : super(key: key);
+
+  final TFormRow? row;
+
+  @override
+  _TFormCellState createState() => _TFormCellState();
+}
+
+class _TFormCellState extends State<TFormCell> {
+  get row => widget.row;
+
+  @override
+  Widget build(BuildContext context) {
+    // cell
+    Widget? widget;
+    if (row.widget != null) {
+      widget = row.widget;
+    } else if (row.widgetBuilder != null) {
+      widget = row.widgetBuilder(context, row);
+    } else {
+      widget = TFormField(row: row);
+    }
+    // background color white default
+    widget = Container(
+      child: widget,
+      color: Colors.white,
+    );
+    // animation
+    widget = row.animation ?? false
+        ? TweenAnimationBuilder(
+            child: widget,
+            duration: Duration(milliseconds: 500),
+            builder: (BuildContext context, dynamic value, Widget? child) {
+              return Opacity(
+                opacity: value,
+                child: child,
+              );
+            },
+            tween: Tween(begin: 0.0, end: 1.0),
+          )
+        : widget;
+    // divider
+    widget = TForm.of(context)!.divider != null && row != TForm.of(context)!.rows!.last
+        ? Column(
+            children: [widget, TForm.of(context)!.divider],
+          )
+        : widget;
+    return widget;
+  }
+}
